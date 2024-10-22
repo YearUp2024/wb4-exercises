564bffb0b7700ddfcd793178e83c02c7dc920ea8
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-22 14:55:35 -0400

    Adding new methods to Room Class

--- wb4demos/HotelOperations/src/main/java/com/pluralsight/Room.java
+++ wb4demos/HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -4,13 +4,13 @@ public class Room {
     private int numberOfBeds;
     private double price;
     private boolean occupied;
-    private boolean dirt;
+    private boolean dirty;
 
-    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirt){
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty){
         this.numberOfBeds = numberOfBeds;
         this.price = price;
         this.occupied = isOccupied;
-        this.dirt = isDirt;
+        this.dirty = isDirty;
     }
 
     public int getNumberOfBeds(){
@@ -24,12 +24,35 @@ public class Room {
     public boolean isOccupied(){
         return occupied;
     }
+    public void setOccupied(boolean occupied){
+        this.occupied = occupied;
+    }
 
     public boolean isDirty(){
-        return dirt;
+        return dirty;
+    }
+    public void setDirty(boolean dirty){
+        this.dirty = dirty;
     }
 
     public boolean isAvailable(){
         return (!this.isOccupied() && !this.isDirty());
+    }
+
+    public boolean checkIn(){
+        return isOccupied();
+    }
+
+    public boolean checkout(){
+        return isAvailable();
+    }
+
+    public boolean cleanroom(){
+        boolean occupied = false;
+        if(checkIn()){
+            setDirty(true);
+            setOccupied(true);
+        }
+        return occupied;
     }
 }
