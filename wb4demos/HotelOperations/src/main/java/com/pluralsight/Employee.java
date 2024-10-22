219120be58e714c3ba22508e3873dc6d5656fbc8
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-22 13:04:09 -0400

    Completing Employee Class

--- wb4demos/HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ wb4demos/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -5,9 +5,61 @@ public class Employee {
     private String name;
     private String department;
     private double payRate;
-    private int hoursWorked;
+    private int regularHoursWorked;
+    private int overTimeHours;
 
-    public Employee(int Em){
+    public Employee(int employeeId, String name, String department, double payRate, int regularHoursWorked, int overTimeHours){
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.regularHoursWorked = regularHoursWorked;
+        this.overTimeHours = overTimeHours;
+    }
 
+    public int getEmployeeId(){
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getName(){
+        return  name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public double getPayRate(){
+        return payRate;
+    }
+    public void setPayRate(double payRate){
+        this.payRate = payRate;
+    }
+
+    public int getRegularHoursWorked(){
+        return regularHoursWorked;
+    }
+    public void setRegularHoursWorked(int regularHoursWorked){
+        this.regularHoursWorked = regularHoursWorked;
+    }
+
+    public int getOverTimeHours(){
+        return overTimeHours;
+    }
+    public void setOverTimeHours(int overTimeHours){
+        this.overTimeHours = overTimeHours;
+    }
+
+    public int getHoursWorked(){
+        return this.regularHoursWorked + this.overTimeHours;
     }
 }
