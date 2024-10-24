ccd0274caf0fbb05d666f3d1c3ed9f633a4c554b
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-24 11:08:30 -0400

    Making main class work

--- wb4demos/BlackJack/src/main/java/com/pluralsight/Main.java
+++ wb4demos/BlackJack/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Deck deck = new Deck();
+        Hand hand = new Hand();
+
+        for(int i = 0; i < 5; i++){
+            Card card = deck.deal();
+            hand.Deal(card);
+        }
+
+        int handValue = hand.getValue();
+        System.out.println("handValue = " + handValue);
     }
 }
