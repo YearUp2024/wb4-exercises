80be8dbb6db5ee423f2b1116865c6500354e6b2a
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-24 10:51:58 -0400

    Completing Hand class

--- wb4demos/BlackJack/src/main/java/com/pluralsight/Hand.java
+++ wb4demos/BlackJack/src/main/java/com/pluralsight/Hand.java
@@ -1,5 +1,29 @@
 package com.pluralsight;
 
-public class Hand {
+import java.util.ArrayList;
 
+public class Hand {
+    private ArrayList<Card> cards;
+
+    public Hand(){
+        this.cards = new ArrayList<>();
+    }
+
+    public void Deal(Card card){
+        cards.add(card);
+    }
+
+    public int getSize(){
+        return cards.size();
+    }
+
+    public int getValue() throws Exception {
+        int hasValue = 0;
+        for(Card card : cards){
+            card.flip();
+            hasValue += card.getPointValue();
+            card.flip();
+        }
+        return hasValue;
+    }
 }
